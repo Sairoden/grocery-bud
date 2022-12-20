9a9ba9262b3675d832fb258dcbad91f6f5a0e0ce
Sairoden <odingandarosa@gmail.com>
2022-12-20 11:32:30 +0800

    remove item and alert

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
 
   const handleSubmit = e => {
     e.preventDefault();
 
     if (!name) {
       // display alert
+      showAlert(true, "Please enter a value", "danger");
     } else if (name && isEditing) {
       // display with edit
     } else {
       // show alert
+      showAlert(true, "Item added to the list", "success");
       const newItem = { id: new Date().getTime().toString(), title: name };
 
       setList([...list, newItem]);
@@ -25,10 +31,24 @@ function App() {
     }
   };
 
+  const showAlert = (show = false, msg = "", type = "") => {
+    setAlert({ show, msg, type });
+  };
+
+  const handleReset = () => {
+    showAlert(true, "empty list", "danger");
+    setList([]);
+  };
+
+  const handleRemoveItem = id => {
+    showAlert(true, "item removed", "danger");
+    setList(list.filter(item => item.id !== id));
+  };
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>Grocery Bud</h3>
         <div className="form-control">
           <input
@@ -45,8 +65,10 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
-          <button className="clear-btn">clear items</button>
+          <List handleRemoveItem={handleRemoveItem} items={list} />
+          <button onClick={handleReset} className="clear-btn">
+            clear items
+          </button>
         </div>
       )}
     </section>
@@ -54,3 +76,5 @@ function App() {
 }
 
 export default App;
+
+// 48
